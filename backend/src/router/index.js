9b011ea6bff3b21ba154d7a9e1d37ac67f710f43
Shahzad <shahzadhere17@gmail.com>
2023-11-16 18:32:01 +0500

    Login Reset Password Request New password forms added

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,5 +1,7 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
 import Login from "../views/Login.vue";
 
 const routes = [
@@ -12,6 +14,22 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
+    },
+    {
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword,
+        meta: {
+            requiresGuest: true
+        }
+    },
+    {
+        path: '/reset-password/:token',
+        name: 'resetPassword',
+        component: ResetPassword,
+        meta: {
+            requiresGuest: true
+        }
     }
 
 ];
