f9fab5b3b5b9803ddc6a4d3fe99fdb863746f338
Shahzad <shahzadhere17@gmail.com>
2024-09-08 23:16:47 +0500

    .

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 
 class ProductResource extends JsonResource
