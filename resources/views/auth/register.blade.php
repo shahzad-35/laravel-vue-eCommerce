bffb020fbcc84eeeb47224acb4c6d96341d86ddb
Shahzad <shahzadhere17@gmail.com>
2024-09-09 23:14:20 +0500

    installed alpine js collapse package and made some changes in blade file

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,73 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<x-app-layout>
+    <form
+        action=""
+        method="post"
+        class="w-[400px] mx-auto p-6 my-16"
+        x-data="signupForm"
+        @submit.prevent="submit"
+    >
+        <h2 class="text-2xl font-semibold text-center mb-4">Create an account</h2>
+        <p class="text-center text-gray-500 mb-3">
+            or
+            <a
+                href="{{ route('login') }}"
+                class="text-sm text-purple-700 hover:text-purple-600"
+            >login with existing account</a
+            >
+        </p>
+        <div class="mb-4">
+            <input
+                placeholder="Your name"
+                type="text"
+                name="name"
+                x-model="form.name"
+                @input="validateName()"
+                class="focus:outline-none  rounded-md w-full"
+                :class="errors.name ? errorClasses : (form.name ? successClasses : defaultClasses)"
+            />
+            <small x-show="errors.name" x-text="errors.name" class="text-red-600"></small>
+        </div>
+        <div class="mb-4">
+            <input
+                placeholder="Your Email"
+                type="email"
+                name="email"
+                x-model="form.email"
+                @input="validateEmail()"
+                class="focus:outline-none  rounded-md w-full"
+                :class="errors.email ? errorClasses : (form.email ? successClasses : defaultClasses)"
+            />
+            <small x-show="errors.email" x-text="errors.email" class="text-red-600"></small>
+        </div>
+        <div class="mb-4">
+            <input
+                placeholder="Password"
+                type="password"
+                name="password"
+                x-model="form.password"
+                @input="validatePassword()"
+                class="border-gray-300 focus:outline-none  rounded-md w-full"
+                :class="errors.password ? errorClasses : (form.password ? successClasses : defaultClasses)"
+            />
+            <small x-show="errors.password" x-text="errors.password" class="text-red-600"></small>
+        </div>
+        <div class="mb-4">
+            <input
+                placeholder="Repeat Password"
+                type="password"
+                name="password"
+                x-model="form.password_repeat"
+                @input="validatePasswordRepeat()"
+                class="border-gray-300 focus:outline-none  rounded-md w-full"
+                :class="errors.password_repeat ? errorClasses : (form.password_repeat ? successClasses : defaultClasses)"
+            />
+            <small x-show="errors.password_repeat" x-text="errors.password_repeat" class="text-red-600"></small>
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
+        <button
+            class="btn-primary bg-emerald-500 hover:bg-emerald-600 active:bg-emerald-700 w-full"
+        >
+            Signup
+        </button>
     </form>
-</x-guest-layout>
+</x-app-layout>
